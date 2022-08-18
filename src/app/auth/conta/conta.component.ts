08a8a8126c1bb174f95064106ba85d162ecbee2c
muri11o <murillofsf@gmail.com>
2022-08-17 23:43:00 -0300

    validação dados ao criar conta

--- src/app/auth/conta/conta.component.ts
+++ src/app/auth/conta/conta.component.ts
@@ -13,24 +13,65 @@ export class ContaComponent implements OnInit {
     this.nome = '';
     this.senha = '';
     this.cadastroRealizdo = false;
-   }
-  
+    this.isValid = true;
+  }
+
   public email: string;
   public confirmarEmail: string;
   public nome: string;
   public senha: string;
   public cadastroRealizdo: boolean;
+  public isValid: boolean;
 
   cadastrar() {
 
+    if (!this.IsValidEmail(this.email, this.confirmarEmail)) {
+      this.isValid = false;
+      if (this.isValid == false)
+        return;
+    }
+
+    if (!this.IsValidSenha(this.senha)) {
+      this.isValid = false;
+      if (this.isValid == false)
+        return;
+    }
+
+
     this.contaService.realizarCadastro(this.email, this.confirmarEmail, this.nome, this.senha)
-    .subscribe();
+      .subscribe();
 
     this.cadastroRealizdo = true;
+    this.isValid = true;
+
   }
 
   ngOnInit(): void {
 
   }
+
+  IsValidEmail(email: string, emailConfirm: string) {
+    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+    if (reg.test(email)) {
+      if (email == emailConfirm) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+  }
+
+  IsValidSenha(senha: string) {
+    if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(\W|_)).{8,}$/.test(senha)) {
+      return true;
+    }
+
+    return false;
+  }
+
 
 }
--- src/appsettings.ts
+++ src/appsettings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-    baseUrlAuth: string = "https://localhost:7088/v1/api/"
-    baseUrlMain: string = "https://localhost:7145/v1/api/"
+    baseUrlAuth: string = "https://busgoianiaauth.azurewebsites.net/v1/api/"
+    baseUrlMain: string = "https://busgoianiamain.azurewebsites.net/v1/api/"
   }
